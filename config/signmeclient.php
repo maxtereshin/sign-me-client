5b89e0a93a2252b23b5038242145fae014fec047
Maxim Tereshin <mterhin@gmail.com>
2023-07-04 16:13:19 +0300

    change config

--- config/signmeclient.php
+++ config/signmeclient.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_key'  => '6WKDMHJKNULEAX93',
+    'api_key'  => env('SIGN_ME_API_KEY'),
     'base_url' => 'https://sign.me/',
     'test_url' => 'https://sandbox.sign.me/',
     'debug'    => false,
